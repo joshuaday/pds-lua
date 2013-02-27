303635d63931265ec81acc4c62bf20c62cb56dc3
Joshua Day <josh.day@gmail.com>
2013-02-26 22:23:10 -0500

    pds

--- pds/layer.lua
+++ pds/layer.lua
@@ -1,92 +1,81 @@
 local ffi = require "ffi"
 
+local layer = { }
+
+local layer_mt = { __index = layer }
+
+function layer:index(x, y)
+	x, y = x - self.x1, y - self.y1
+	if x < 0 or y < 0 or x >= self.width or y >= self.height then
+		return 0
+	else
+		return 1 + x + y * self.width
+	end
+end
+
+function layer:get(x, y)
+	return self.cells[self:index(x, y)]
+end
+
+function layer:set(x, y, v)
+	x, y = x - self.x1, y - self.y1
+	if x < 0 or y < 0 or x >= self.width or y >= self.height then
+		return
+	end
+
+	self.cells[1 + x + y * self.width] = v
+	return self
+end
+
+function layer:moveto(x, y)
+	x1, y1 = x, y
+	self.x1, self.y1, self.x2, self.y2 = x1, y1, x1 + width - 1, y1 + height - 1
+	return self
+end
+
+function layer:recenter(x, y)
+	x1, y1 = x - math.floor(self.width / 2), y - math.floor(self.height / 2)
+	self.x1, self.y1, self.x2, self.y2 = x1, y1, x1 + self.width - 1, y1 + self.height - 1
+	return self
+end
+
+function layer:fill(v)
+	for i = 1, self.width * self.height do
+		cells[i] = v
+	end
+	return self
+end
+
+function layer:setdefault(v)
+	self.cells[0] = v
+	return self
+end
+
+function layer:each(f)
+	local i = 1
+	for y = self.y1, self.y2 do
+		for x = self.x1, self.x2 do
+			f(self.cells[i], x, y)
+			i = i + 1
+		end
+	end
+	return self
+end
+
+
 local function new_layer(ctype, width, height) 
-	local self
-
-	local x1, y1, default = 1, 1, 0
-	local cells = ffi.new(ctype .. "[?]", 1 + width * height)
-
-	local function index(x, y)
-		x, y = x - x1, y - y1
-		if x < 0 or y < 0 or x >= width or y >= height then
-			return 0
-		else
-			return 1 + x + y * width
-		end
-	end
-
-	local function get(x, y)
-		return cells[index(x, y)]
-	end
-
-	local function set(x, y, v)
-		x, y = x - x1, y - y1
-		if x < 0 or y < 0 or x >= width or y >= height then
-			return
-		end
-
-		cells[1 + x + y * width] = v
-		return self
-	end
-
-	local function moveto(x, y)
-		x1, y1 = x, y
-		self.x1, self.y1, self.x2, self.y2 = x1, y1, x1 + width - 1, y1 + height - 1
-		return self
-	end
-
-	local function recenter(x, y)
-		x1, y1 = x - math.floor(width / 2), y - math.floor(height / 2)
-		self.x1, self.y1, self.x2, self.y2 = x1, y1, x1 + width - 1, y1 + height - 1
-		return self
-	end
-
-	local function fill(v)
-		for i = 1, width * height do
-			cells[i] = v
-		end
-		return self
-	end
-	
-	local function setdefault(v)
-		cells[0] = v
-		return self
-	end
-
-	local function each(f)
-		local i = 1
-		for y = y1, y1 + height - 1 do
-			for x = x1, x1 + width - 1 do
-				f(cells[i], x, y)
-				i = i + 1
-			end
-		end
-		return self
-	end
-	
-	self = {
-		moveto = moveto,
-		recenter = recenter,
-
-		each = each,
-		cells = cells,
-		index = index,
-		get = get,
-		set = set,
-		setdefault = setdefault,
-		fill = fill,
-
-		ctype = ctype,
-
+	return setmetatable({
+		x1 = 1,
+		y1 = 1,
+		x2 = x1 + width - 1,
+		y2 = y1 + height - 1,
 		width = width,
 		height = height,
 
-		-- read only:
-		x1 = x1, y1 = y1,
-		x2 = x1 + width - 1, y2 = y1 + height - 1
-	}
+		cells = ffi.new(ctype .. "[?]", 1 + width * height)
 
-	return self
+		ctype = ctype
+	}, layer_mt)
 end
 
 return {
